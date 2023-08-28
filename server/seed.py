d9c7a3e8ea79387e8aec579b77c62f3cd7a7605f
Jerricke <eejerrickw@gmail.com>
2023-08-28 15:06:36 -0600

    models and base GET routes complete

--- server/seed.py
+++ server/seed.py
@@ -10,7 +10,7 @@ import datetime
 
 # Local imports
 from app import app
-from models import db, User, Trip, Signup
+from models import db, User, Trip, Signup, TripComment, CommunityComment
 
 if __name__ == '__main__':
     fake = Faker()
@@ -32,7 +32,7 @@ if __name__ == '__main__':
                 age = randint(18, 65),
                 location = fake.city(),
                 distance_traveled = randint(10, 1000),
-                personal_bio = fake.text(max_nb_chars=20),
+                personal_bio = fake.text(max_nb_chars=100),
             )
             user.password_hash = fake.password()
             db.session.add(user)
@@ -47,7 +47,7 @@ if __name__ == '__main__':
             trip = Trip(
                 name = fake.name(),
                 owner_id = random.choice(users).id,
-                description = fake.text(max_nb_chars=20),
+                description = fake.text(max_nb_chars=100),
                 location = fake.city(),
                 distance = randint(10, 100),
                 time_start = tStart,
@@ -68,4 +68,28 @@ if __name__ == '__main__':
             db.session.commit()
             signups.append(signup)
 
+        print("seeding trip comments")
+        trip_comments = []
+        for _ in range(30):
+            trip_comments = TripComment(
+                content = fake.text(max_nb_chars=100),
+                user_id = random.choice(users).id,
+                trip_id = random.choice(trips).id,
+            )
+            db.session.add(trip_comments)
+            db.session.commit()
+            signups.append(trip_comments)
+
+        print("seeding community comments")
+        community_comments = []
+        for _ in range(30):
+            community_comment = CommunityComment(
+                content = fake.text(max_nb_chars=100),
+                user_id = random.choice(users).id,
+            )
+            db.session.add(community_comment)
+            db.session.commit()
+            signups.append(community_comment)
+
         print("seeding complete")
+
